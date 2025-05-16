92b6fe64cf09163bed74d0c682e696a64d964e9f
LibaMariyamK <156946460+LibaMariyamK@users.noreply.github.com>
2025-05-16 21:04:35 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,3 +9,15 @@ weight = st.number_input("Enter your weight (in kg):")
 if st.button("Calculate BMI"):
     bmi = weight / ((height / 100) ** 2)
     st.write(f"Your BMI is: {bmi:.2f}")
+    if bmi<=18.5:
+        st.write("UnderUnderweight") 
+    elif 18.5<bmi<=25:
+        st.write("Healthy Weight")
+    elif 25 < bmi <= 30:
+        st.write("OverWeight")  
+    else:
+        st.write("Obesity")   
+          
+            
+    
+    
